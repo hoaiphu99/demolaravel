715b825d2f6ff782fc328e25a31892918a01e721
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-03-11 11:25:23 +0700

    Config DB Connect to Heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://euvqvxcesjidok:3fd0b5c0e1fe2835574e7ce54a6c73a31875cd7892173eee4b7a995a5c6f7f71@ec2-54-164-22-242.compute-1.amazonaws.com:5432/d4ilt8lbht71i7');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +67,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'url' => ltrim($DATABASE_URL["path"], "/"),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
