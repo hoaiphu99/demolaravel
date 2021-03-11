63592e55b3da51052f660495c94faf539b133f58
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-03-11 22:40:34 +0700

    Update user.php

--- resources/views/user.php
+++ resources/views/user.php
@@ -4,38 +4,56 @@
 	<title> Document  </title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 	
-	<link rel="stylesheet" href="vendor/bootstrap.css">
-	<link rel="stylesheet" href="vendor/angular-material.min.css">
-	<link rel="stylesheet" href="vendor/font-awesome.css">
-	<link rel="stylesheet" href="1.css">
+	<link rel="stylesheet" href="<?php echo URL::to('/'); ?>/vendor/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo URL::to('/'); ?>/vendor/angular-material.min.css">
+	<link rel="stylesheet" href="<?php echo URL::to('/'); ?>/vendor/font-awesome.css">
+	<link rel="stylesheet" href="<?php echo URL::to('/'); ?>/1.css">
 </head>
 <body ng-app="myApp" ng-controller="MyController">
 
-	<div class="jumbotron">
-		<h1 class="display-4">Các hàm cơ bản cho single page</h1>
-		<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-		<hr class="my-4">
-		<div class="menu">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<div class="nav">
-					<div class="navbar-nav">
-						<a class="nav-item nav-link" href="#">Mail chính</a>
-						<a class="nav-item nav-link" href="#">Xã hội</a>
-						<a class="nav-item nav-link" href="#">Quảng cáo</a>
+	<div class="inra">
+		<div class="container">
+			<div class="jumbotron">
+				<h1 class="display-4">Hello, world!</h1>
+				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+				<hr class="my-4">
+				<ul>
+					<div ng-repeat="motnguoi in nhieunguoi" ng-init="motnguoi.hienra=false">
+						<div class="card bg-primary|secondary|success|danger|warning|info|light|dark border-primary|secondary|success|danger|warning|info|light|dark sua" ng-show="motnguoi.hienra">
+							<div class="card-header">
+							Thông tin về <input type="text" class="form-control" ng-model="motnguoi.ten">
+							</div>
+							<div class="card-body pl-1 pr-1 pt-1 pb-1">
+								<b>Năm sinh: </b> <input type="text" class="form-control" ng-model="motnguoi.namsinh"> <br>
+								<b>Facebook: </b> <input type="text" class="form-control" ng-model="motnguoi.facebook"> <br>
+								<b>Điện thoại: </b> <input type="text" class="form-control" ng-model="motnguoi.dienthoai"> <br>
+								<b class="float-xs-center btn btn-danger btn-block" ng-click="sua(motnguoi)">Lưu</b>
+							</div>
+						</div>
+						<div class="card bg-primary|secondary|success|danger|warning|info|light|dark border-primary|secondary|success|danger|warning|info|light|dark" ng-show="!motnguoi.hienra">
+							<div class="card-header">
+								<b class="float-xs-right"><i class="fa fa-pencil" ng-click="sua(motnguoi)"></i></b>
+							Thông tin về {{motnguoi.ten}}</div>
+							<div class="card-body text-primary|secondary|success|danger|warning|info|light|dark pl-1 pr-1 pt-1 pb-1">
+								<b>Năm sinh: </b> <i>{{motnguoi.namsinh}}</i> <br>
+								<b>Facebook: </b> <i>{{motnguoi.facebook}}</i> <br>
+								<b>Điện thoại: </b> <i>{{motnguoi.dienthoai}}</i> <br>
+							</div>
+						</div>
 					</div>
-				</div>
-			</nav>
+				</ul>
+			</div>
 		</div>
 	</div>
 
 
 
-	<script type="text/javascript" src="vendor/bootstrap.js"></script>  
-	<script type="text/javascript" src="vendor/angular-1.5.min.js"></script>  
-	<script type="text/javascript" src="vendor/angular-animate.min.js"></script>
-	<script type="text/javascript" src="vendor/angular-aria.min.js"></script>
-	<script type="text/javascript" src="vendor/angular-messages.min.js"></script>
-	<script type="text/javascript" src="vendor/angular-material.min.js"></script>  
-	<script type="text/javascript" src="1.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/bootstrap.js"></script>  
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-1.5.min.js"></script>  
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-animate.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-aria.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-messages.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-material.min.js"></script>  
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/1.js"></script>
 </body>
 </html>
