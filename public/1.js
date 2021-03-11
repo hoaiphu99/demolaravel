3951669ec965045a3cc82f65b16eeff672503ce9
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-03-11 22:38:16 +0700

    test user

---
+++ public/1.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module('myApp',['ngMaterial']);
+app.controller('MyController',  function($scope){
+
+	$scope.sua = function(x) {
+		x.hienra = !x.hienra;
+	}
+	
+	$scope.nhieunguoi = [
+		{
+			ten: "Viet",
+			namsinh: "1986",
+			facebook: "fb.com/nddviet",
+			dienthoai: "0123456789"
+		},
+
+		{
+			ten: "Nam",
+			namsinh: "1991",
+			facebook: "fb.com/nam",
+			dienthoai: "0123654234"
+		},
+
+		{
+			ten: "Han",
+			namsinh: "1999",
+			facebook: "fb.com/han",
+			dienthoai: "03213848123"
+		}
+
+	]
+})
---
+++ resources/views/user.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en"  >
+<head>
+	<title> Document  </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 	
+	<link rel="stylesheet" href="vendor/bootstrap.css">
+	<link rel="stylesheet" href="vendor/angular-material.min.css">
+	<link rel="stylesheet" href="vendor/font-awesome.css">
+	<link rel="stylesheet" href="1.css">
+</head>
+<body ng-app="myApp" ng-controller="MyController">
+
+	<div class="jumbotron">
+		<h1 class="display-4">Các hàm cơ bản cho single page</h1>
+		<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+		<hr class="my-4">
+		<div class="menu">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="nav">
+					<div class="navbar-nav">
+						<a class="nav-item nav-link" href="#">Mail chính</a>
+						<a class="nav-item nav-link" href="#">Xã hội</a>
+						<a class="nav-item nav-link" href="#">Quảng cáo</a>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</div>
+
+
+
+	<script type="text/javascript" src="vendor/bootstrap.js"></script>  
+	<script type="text/javascript" src="vendor/angular-1.5.min.js"></script>  
+	<script type="text/javascript" src="vendor/angular-animate.min.js"></script>
+	<script type="text/javascript" src="vendor/angular-aria.min.js"></script>
+	<script type="text/javascript" src="vendor/angular-messages.min.js"></script>
+	<script type="text/javascript" src="vendor/angular-material.min.js"></script>  
+	<script type="text/javascript" src="1.js"></script>
+</body>
+</html>
