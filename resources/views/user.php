f729c2919e37d60e8018229f919ddcbbee37c1b1
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-03-12 08:13:32 +0700

    css

--- resources/views/user.php
+++ resources/views/user.php
@@ -4,11 +4,11 @@
 	<title> Document  </title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 	
-	<link rel="stylesheet" href="../resource/vendor/bootstrap.css">
-	<link rel="stylesheet" href="../resource/vendor/angular-material.min.css">
-	<link rel="stylesheet" href="../resource/vendor/font-awesome.css">
-	<link rel="stylesheet" href="../resource/1.css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"></script>
+	<link rel="stylesheet" href="{{ secure_asset('vendor/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('vendor/angular-material.min.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('vendor/font-awesome.css') }}">
+	<link rel="stylesheet" href="{{ secure_asset('1.css') }}">
+    <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"></script> -->
 
     <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
@@ -22,7 +22,7 @@
 				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 				<hr class="my-4">
 				<ul>
-					<div ng-repeat="motnguoi in nhieunguoi" ng-init="motnguoi.hienra=false">
+					<!-- <div ng-repeat="motnguoi in nhieunguoi" ng-init="motnguoi.hienra=false">
 						<div class="card bg-primary|secondary|success|danger|warning|info|light|dark border-primary|secondary|success|danger|warning|info|light|dark sua" ng-show="motnguoi.hienra">
 							<div class="card-header">
 							Thông tin về <input type="text" class="form-control" ng-model="motnguoi.ten">
@@ -44,7 +44,7 @@
 								<b>Điện thoại: </b> <i>{{motnguoi.dienthoai}}</i> <br>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</ul>
 			</div>
 		</div>
@@ -52,12 +52,12 @@
 
 
 
-	<script type="text/javascript" src="../resource/vendor/bootstrap.js"></script>  
-	<script type="text/javascript" src="../resource/vendor/angular-1.5.min.js"></script>  
-	<script type="text/javascript" src="../resource/vendor/angular-animate.min.js"></script>
-	<script type="text/javascript" src="../resource/vendor/angular-aria.min.js"></script>
-	<script type="text/javascript" src="../resource/vendor/angular-messages.min.js"></script>
-	<script type="text/javascript" src="../resource/vendor/angular-material.min.js"></script>  
-	<script type="text/javascript" src="../resource/1.js"></script>
+	<script type="text/javascript" src="{{ secure_asset('vendor/bootstrap.js') }}"></script>  
+	<script type="text/javascript" src="{{ secure_asset('vendor/angular-1.5.min.js') }}"></script>  
+	<script type="text/javascript" src="{{ secure_asset('vendor/angular-animate.min.js') }}"></script>
+	<script type="text/javascript" src="{{ secure_asset('vendor/angular-aria.min.js') }}"></script>
+	<script type="text/javascript" src="{{ secure_asset('vendor/angular-messages.min.js') }}"></script>
+	<script type="text/javascript" src="{{ secure_asset('vendor/angular-material.min.js') }}"></script>  
+	<script type="text/javascript" src="{{ secure_asset('resource/1.js') }}"></script>
 </body>
 </html>
