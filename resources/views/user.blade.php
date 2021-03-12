ef53ed24028cc853e2ed5d57101b4889484e9a5d
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-03-12 09:55:53 +0700

    Add DB

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,10 +4,10 @@
 	<title> Document  </title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 	
-	<link rel="stylesheet" href="{{ secure_asset('vendor/bootstrap.css') }}">
-	<link rel="stylesheet" href="{{ secure_asset('vendor/angular-material.min.css') }}">
-	<link rel="stylesheet" href="{{ secure_asset('vendor/font-awesome.css') }}">
-	<link rel="stylesheet" href="{{ secure_asset('css/1.css') }}">
+	<link rel="stylesheet" href="<?php echo URL::to('/'); ?>/vendor/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/vendor/angular-material.min.css">
+    <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/vendor/font-awesome.css">
+    <link rel="stylesheet" href="<?php echo URL::to('/'); ?>/css/1.css">
 
 </head>
 <body ng-app="myApp" ng-controller="MyController">
@@ -19,26 +19,27 @@
 				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 				<hr class="my-4">
 				<ul>
-					<div ng-repeat="motnguoi in nhieunguoi" ng-init="motnguoi.hienra=false">
-						<div class="card bg-primary|secondary|success|danger|warning|info|light|dark border-primary|secondary|success|danger|warning|info|light|dark sua" ng-show="motnguoi.hienra">
+					<div ng-repeat="u in user" ng-init="u.hienra=false">
+						<div class="card bg-primary|secondary|success|danger|warning|info|light|dark border-primary|secondary|success|danger|warning|info|light|dark sua" ng-show="u.hienra">
 							<div class="card-header">
-							Thông tin về <input type="text" class="form-control" ng-model="motnguoi.ten">
+								Thông tin về <input type="text" class="form-control" ng-model="u.name">
 							</div>
 							<div class="card-body pl-1 pr-1 pt-1 pb-1">
-								<b>Năm sinh: </b> <input type="text" class="form-control" ng-model="motnguoi.namsinh"> <br>
-								<b>Facebook: </b> <input type="text" class="form-control" ng-model="motnguoi.facebook"> <br>
-								<b>Điện thoại: </b> <input type="text" class="form-control" ng-model="motnguoi.dienthoai"> <br>
-								<b class="float-xs-center btn btn-danger btn-block" ng-click="sua(motnguoi)">Lưu</b>
+								<input type="hidden" class="form-control" ng-model="u.id"> <br>
+								<b>Năm sinh: </b> <input type="text" class="form-control" ng-model="u.birthday"> <br>
+								<b>Facebook: </b> <input type="text" class="form-control" ng-model="u.facebook"> <br>
+								<b>Điện thoại: </b> <input type="text" class="form-control" ng-model="u.phone"> <br>
+								<b class="float-xs-center btn btn-danger btn-block" ng-click="update(u)">Lưu</b>
 							</div>
 						</div>
-						<div class="card bg-primary|secondary|success|danger|warning|info|light|dark border-primary|secondary|success|danger|warning|info|light|dark" ng-show="!motnguoi.hienra">
+						<div class="card bg-primary|secondary|success|danger|warning|info|light|dark border-primary|secondary|success|danger|warning|info|light|dark" ng-show="!u.hienra">
 							<div class="card-header">
-								<b class="float-xs-right"><i class="fa fa-pencil" ng-click="sua(motnguoi)"></i></b>
-							Thông tin về <%motnguoi.ten%></div>
+								<b class="float-xs-right"><i class="fa fa-pencil" ng-click="sua(u)"></i></b>
+								Thông tin về <%u.name%></div>
 							<div class="card-body text-primary|secondary|success|danger|warning|info|light|dark pl-1 pr-1 pt-1 pb-1">
-								<b>Năm sinh: </b> <i><%motnguoi.namsinh%></i> <br>
-								<b>Facebook: </b> <i><%motnguoi.facebook%></i> <br>
-								<b>Điện thoại: </b> <i><%motnguoi.dienthoai%></i> <br>
+								<b>Năm sinh: </b> <i><%u.birthday%></i> <br>
+								<b>Facebook: </b> <i><%u.facebook%></i> <br>
+								<b>Điện thoại: </b> <i><%u.phone%></i> <br>
 							</div>
 						</div>
 					</div>
@@ -49,12 +50,12 @@
 
 
 
-	<script type="text/javascript" src="{{ secure_asset('vendor/bootstrap.js') }}"></script>  
-	<script type="text/javascript" src="{{ secure_asset('vendor/angular-1.5.min.js') }}"></script>  
-	<script type="text/javascript" src="{{ secure_asset('vendor/angular-animate.min.js') }}"></script>
-	<script type="text/javascript" src="{{ secure_asset('vendor/angular-aria.min.js') }}"></script>
-	<script type="text/javascript" src="{{ secure_asset('vendor/angular-messages.min.js') }}"></script>
-	<script type="text/javascript" src="{{ secure_asset('vendor/angular-material.min.js') }}"></script>  
-	<script type="text/javascript" src="{{ secure_asset('js/1.js') }}"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/bootstrap.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-1.5.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-animate.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-aria.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-messages.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/vendor/angular-material.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL::to('/'); ?>/js/1.js"></script>
 </body>
 </html>
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('user', 'App\Http\Controllers\Api\UserController@index');
+
+Route::put('user/{user}', 'App\Http\Controllers\Api\UserController@update');
