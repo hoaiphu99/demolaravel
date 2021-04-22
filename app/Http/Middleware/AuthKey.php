4a41028046d2b0401a1fedb629c7c6434593f291
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-04-22 22:07:14 +0700

    xxx

--- app/Http/Middleware/AuthKey.php
+++ app/Http/Middleware/AuthKey.php
@@ -24,7 +24,7 @@ class AuthKey
         ;
         $token = $request->header('API_KEY');
         echo $token;
-        if($request->headers->contains('API_KEY', 'PHU')) {
+        if(!$request->headers->contains('API_KEY', 'PHU')) {
             return response()->json(['message' => 'App key not found'.$token." !"], 401);
         }
         return $next($request);
