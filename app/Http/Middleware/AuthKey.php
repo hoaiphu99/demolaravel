0635de0e8f9a2b5f2f46c0edad2deaa8d4c111eb
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-04-22 21:36:27 +0700

    xxx

--- app/Http/Middleware/AuthKey.php
+++ app/Http/Middleware/AuthKey.php
@@ -22,7 +22,7 @@ class AuthKey
 //          return response()->json(['message' => 'No token found! '.$token], 401);
 //        }
         $token = $request->header('APP_KEY');
-        if($token != 'PHU') {
+        if($token == 'PHU') {
             return response()->json(['message' => 'App key not found'], 401);
         }
         return $next($request);
