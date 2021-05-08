e4815a197d5551f1c164eb33f3557486ee6f8df7
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-08 19:11:32 +0700

    update db

---
+++ database/migrations/2021_05_08_121113_edit_table_posts_2.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditTablePosts2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->renameColumn('description', 'content');
+            $table->dropColumn('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
