04ebe471484869543f5362bd4fd57f8fb51be2b4
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-13 15:13:15 +0700

    zz

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -67,7 +67,7 @@ class PostController extends Controller
     {
         $post = Post::create($request->all());
         //$decode_data = base64_decode($request->get('image'));
-        $newImage = $this->saveImgBase64($request->get('image'), 'assets/images');
+        $newImage = $this->saveImgBase64($request->image, 'assets/images');
         //$img = $request->file('image');
 //        $base64String = 'data:image/' . $img->getClientOriginalExtension() . ';base64,' . base64_encode($img);
 //        $arr = explode(';', $base64String);
