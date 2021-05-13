80bd8c490bbf113ee6310b6551c01d24b2879c7e
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-13 15:55:12 +0700

    xx

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
 
             ],
             'form_params' => [
-                'image' => $request->file('image')
+                'image' => base64_encode($request->file('image'))
             ]
         ]);
         dd($image->getBody()->getContents());
