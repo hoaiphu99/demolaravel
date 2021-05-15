cdd324aacbfe8ee9fd4a3d27b60b29d1a154af80
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-15 13:21:49 +0700

    fix

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,9 +19,9 @@ class UserResource extends JsonResource
             'username' => $this->username,
             // 'password' => $this->password,
             'name' => $this->name,
-            // 'email' => $this->email,
-            // 'phone' => $this->phone,
-            // 'birthday' => $this->birthday
+             'email' => $this->email,
+             'phone' => $this->phone,
+             'birthday' => $this->birthday
         ];
     }
 }
