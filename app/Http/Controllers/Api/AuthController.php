ae67a1dc4929a07709dc518897272272e265d090
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-20 18:14:50 +0700

    update login api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Config;
 
 class AuthController extends Controller
 {
     public function login(Request $request) {
         $user = User::where(['username' => $request->get('username')])->first();
         //dd($request->get('password'));
+        if($user == null)
+            return response()->json(['status' => Config::get('siteMsg.fails_code'), 'data' => null]);
 
-        return response()->json(['status' => 1, 'data' => $user]);
+        return response()->json(['status' => Config::get('siteMsg.success_code'), 'data' => $user]);
     }
 }
