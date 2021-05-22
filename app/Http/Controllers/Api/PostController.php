cb12aa1fc87531fd1a8b7d2eccf1909035b97a8d
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-22 09:45:21 +0700

    sort post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -22,7 +22,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $post = Post::all();
+        $post = Post::all()->sortDesc();
 
         return response()->json(['status' => 1, 'data' => PostResource::collection($post)]);
     }
