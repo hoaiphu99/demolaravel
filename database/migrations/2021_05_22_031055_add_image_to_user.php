4dec2203900794b32e52676875024571ba52790c
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-22 10:14:38 +0700

    add image to user

---
+++ database/migrations/2021_05_22_031055_add_image_to_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImageToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('User', function (Blueprint $table) {
+            $table->string('avatar', 500)->nullable();
+            $table->string('utype')->default('USR')->nullable()->comment('ADM for Admin and USR for User');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('User', function (Blueprint $table) {
+            //
+        });
+    }
+}
