6f432b8734129953032436ca68719b0d7296dbd8
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-24 05:11:19 +0700

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
                 'password' => $password
             ]
         ]);
-        $status = $response->getBody()->getContents();
+        $status = $response->getBody();
 
         dd($status);
         //return $a = $response->getBody()->getContents();
