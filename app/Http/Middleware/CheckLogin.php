49f61d39f891f286aa3fb2c8ed56eb862fb0eb1a
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-25 11:46:18 +0700

    fix

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use mysql_xdevapi\Exception;
 
 class CheckLogin
 {
@@ -14,8 +15,19 @@ class CheckLogin
      * @param  \Closure  $next
      * @return mixed
      */
+    protected $except =[
+        'login',
+        'logout',
+        'signup'
+    ];
+
     public function handle(Request $request, Closure $next)
     {
+        foreach ($this->except as $excluded_route) {
+            if ($request->path() === $excluded_route) {
+                return $next($request);
+            }
+        }
         if(!$request->session()->has('user'))
             return redirect(route('login'));
         return $next($request);
