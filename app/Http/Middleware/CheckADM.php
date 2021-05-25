9637f44ac72612b0dd8c5bb943c5ce8a09d402bd
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-25 13:48:02 +0700

    check admin

---
+++ app/Http/Middleware/CheckADM.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckADM
+{
+    protected $except =[
+        'login',
+        'logout',
+        'signup'
+    ];
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        foreach ($this->except as $excluded_route) {
+            if ($request->path() === $excluded_route) {
+                return $next($request);
+            }
+        }
+        if(!$request->session()->has('user')){
+            if (session()->get('user')->utype == 'USR')
+                return redirect(route('index'));
+        }
+        return $next($request);
+    }
+}
