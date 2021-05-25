388173689f3d68e70e618859a05761b3683ddff3
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-25 14:33:39 +0700

    fix icon

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -705,7 +705,7 @@
 <script src="{{ secure_asset('assets/user/js/bootstrap-datepicker.min.js')}}"></script>
 <script src="{{ secure_asset('assets/user/js/swiper.min.js')}}"></script>
 <script src="{{ secure_asset('assets/user/js/aos.js')}}"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="{{ secure_asset('assets/user/js/picturefill.min.js')}}"></script>
 <script src="{{ secure_asset('assets/user/js/lightgallery-all.min.js')}}"></script>
 <script src="{{ secure_asset('assets/user/js/jquery.mousewheel.min.js')}}"></script>
