e3a60a00aa961b9881b0f422b55831741dede6d3
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-26 14:17:07 +0700

    update

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -258,157 +258,14 @@
                     </form>
                 </div>
             </li>
-            <li><a href="newsfeed.html" title="Home" data-ripple=""><i class="ti-home"></i></a></li>
-            <li>
-                <a href="#" title="Notification" data-ripple="">
-                    <i class="ti-bell"></i><span>20</span>
-                </a>
-                <div class="dropdowns">
-                    <span>4 New Notifications</span>
-                    <ul class="drops-menu">
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-1.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>sarah Loren</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag green">New</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-2.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Jhon doe</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag red">Reply</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-3.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Andrew</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag blue">Unseen</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-4.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Tom cruse</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag">New</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-5.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Amy</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag">New</span>
-                        </li>
-                    </ul>
-                    <a href="notifications.html" title="" class="more-mesg">view more</a>
-                </div>
-            </li>
-            <li>
-                <a href="#" title="Messages" data-ripple=""><i class="ti-comment"></i><span>12</span></a>
-                <div class="dropdowns">
-                    <span>5 New Messages</span>
-                    <ul class="drops-menu">
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-1.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>sarah Loren</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag green">New</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-2.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Jhon doe</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag red">Reply</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-3.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Andrew</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag blue">Unseen</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-4.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Tom cruse</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag">New</span>
-                        </li>
-                        <li>
-                            <a href="notifications.html" title="">
-                                <img src="{{secure_asset('assets/user/images/resources/thumb-5.jpg')}}" alt="">
-                                <div class="mesg-meta">
-                                    <h6>Amy</h6>
-                                    <span>Hi, how r u dear ...?</span>
-                                    <i>2 min ago</i>
-                                </div>
-                            </a>
-                            <span class="tag">New</span>
-                        </li>
-                    </ul>
-                    <a href="messages.html" title="" class="more-mesg">view more</a>
-                </div>
-            </li>
-            <li><a href="#" title="Languages" data-ripple=""><i class="fa fa-globe"></i></a>
-                <div class="dropdowns languages">
-                    <a href="#" title=""><i class="ti-check"></i>English</a>
-                    <a href="#" title="">Arabic</a>
-                    <a href="#" title="">Dutch</a>
-                    <a href="#" title="">French</a>
-                </div>
-            </li>
+            <li><a href="{{route('index')}}" title="Home" data-ripple=""><i class="ti-home"></i></a></li>
         </ul>
         <div class="user-img">
-            <img height="45px" src="{{secure_asset('assets/user/images/logo.png')}}" alt="">
+            <img height="45px" src="{{session()->get('user')->avatar}}" alt="">
             <span class="status f-online"></span>
             <div class="user-setting">
-                <a href="#" title=""><span class="status f-online"></span>online</a>
-                <a href="#" title=""><span class="status f-away"></span>away</a>
-                <a href="#" title=""><span class="status f-off"></span>offline</a>
-                <a href="#" title=""><i class="ti-user"></i> view profile</a>
+                <a href="{{route('profile.username', ['username' => session()->get('username')->username])}}" title=""><i class="ti-user"></i> view profile</a>
                 <a href="#" title=""><i class="ti-pencil-alt"></i>edit profile</a>
-                <a href="#" title=""><i class="ti-target"></i>activity log</a>
-                <a href="#" title=""><i class="ti-settings"></i>account setting</a>
                 <a href="{{route('logout')}}" title=""><i class="ti-power-off"></i>log out</a>
             </div>
         </div>
@@ -682,7 +539,7 @@
 <script src="{{secure_asset('assets/user/js/script.js')}}"></script>
 <script src="{{secure_asset('assets/user/js/map-init.js')}}"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8c55_YHLvDHGACkQscgbGLtLRdxBDCfI"></script>
-  <script type="text/javascript" src="{{ secure_asset('vendor/bootstrap.js') }}"></script>
+<script type="text/javascript" src="{{ secure_asset('vendor/bootstrap.js') }}"></script>
 {{--  <script type="text/javascript" src="{{ secure_asset('vendor/angular-1.5.min.js') }}"></script>--}}
 {{--  <script type="text/javascript" src="{{ secure_asset('vendor/angular-animate.min.js') }}"></script>--}}
 {{--  <script type="text/javascript" src="{{ secure_asset('vendor/angular-aria.min.js') }}"></script>--}}
