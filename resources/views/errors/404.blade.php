7fd96518b7931d15401f36a7031c62a3412bf2e9
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-26 15:30:30 +0700

    update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -22,7 +22,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="error-page">
-                    <div class="bg-image" style="background-image: url(images/resources/404.jpg)"></div>
+                    <div class="bg-image" style="background-image: url(assets/user/images/resources/404.jpg)"></div>
                     <div class="error-meta">
                         <h1>whoops!</h1>
                         <span>we couldn't find that page </span>
