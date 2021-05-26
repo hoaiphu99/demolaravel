7206b87cd1100b566af247ec15bd542878fda3c0
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-26 15:28:48 +0700

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>Wibu Social Network Media</title>
+    <link rel="icon" href="{{secure_asset('assets/user/images/fav.png" type="image/png')}}" sizes="16x16">
+
+    <link rel="stylesheet" href="{{secure_asset('assets/user/css/main.min.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/user/css/style.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/user/css/color.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('assets/user/css/responsive.css')}}">
+
+</head>
+<body>
+<!--<div class="se-pre-con"></div>-->
+<div class="theme-layout">
+    <div class="container-fluid pdng0">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="error-page">
+                    <div class="bg-image" style="background-image: url(images/resources/404.jpg)"></div>
+                    <div class="error-meta">
+                        <h1>whoops!</h1>
+                        <span>we couldn't find that page </span>
+                        <a href="{{route('index')}}" title="" data-ripple="">Go Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="{{secure_asset('assets/user/js/main.min.js')}}"></script>
+<script src="{{secure_asset('assets/user/js/script.js')}}"></script>
+
+</body>
+
+</html>
