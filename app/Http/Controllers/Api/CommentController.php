95153c645b44180f317b66e226b0a842a1074fe8
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-05-26 17:06:38 +0700

    test

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -41,9 +41,8 @@ class CommentController extends Controller
         // update lai so comment
         $comment = json_decode($comment);
         $post = Post::where(['id' => $comment->post_id])->first();
-        $cmt_count = $post->comment_count++;
-        dd($cmt_count);
-        $post->update(['comment_count' => $cmt_count]);
+        $cmt_count = $post->comment_count;
+        $post->update(['comment_count' => ++$cmt_count]);
 //        $post_client = new Client(['base_uri' => Config::get('siteVars.API_URL')]);
 //        $post_response = $post_client->get('post/'.$comment->post_id, [
 //            'headers' => [
