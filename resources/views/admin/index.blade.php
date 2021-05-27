028ba5367bae2fb24bdce1afe1012f63704c774f
Phulegia2020 <67860693+Phulegia2020@users.noreply.github.com>
2021-05-27 19:34:54 +0700

    Fix
    Fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,9 +3,33 @@
 @section('content')
 
 <head>
-    <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/icon/themify-icons/themify-icons.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/icon/themify-icons/themify-icons.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css"> -->
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css"> -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="CodedThemes">
+    <meta name="keywords" content=" Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+    <meta name="author" content="CodedThemes">
+    <!-- Favicon icon -->
+    <link rel="icon" href="{{ secure_asset('assets/images/favicon.ico') }}" type="image/x-icon">
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+    <!-- Required Fremwork -->
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/bootstrap/css/bootstrap.min.css') }}">
+    <!-- themify-icons line icon -->
+    <!-- <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/icon/themify-icons/themify-icons.css') }}"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <!-- ico font -->
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/icon/icofont/css/icofont.css') }}">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/jquery.mCustomScrollbar.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/main.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('vendor/angular-material.min.css') }}">
 </head>
 
 <div class="page-body">
