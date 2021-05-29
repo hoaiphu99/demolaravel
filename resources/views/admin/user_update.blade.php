fea45a7e8ab482a549c498f454c2cbcb31b80853
Phulegia2020 <67860693+Phulegia2020@users.noreply.github.com>
2021-05-29 15:54:09 +0700

    Fix

--- resources/views/admin/user_update.blade.php
+++ resources/views/admin/user_update.blade.php
@@ -46,10 +46,11 @@
                             <div class="inbox-details-default">
                                 <div class="inbox-details-heading">Form</div>
                                 <div class="inbox-details-body">
-                                    {{--<div class="alert alert-info">${message} {{ route('user.update', $user_detail->id) }} {{$user_detail->id}}</div>--}}
-                                    <form action="#" class="com-mail" method="POST">
+                                    {{--<div class="alert alert-info">${message}  </div>--}}
+                                    <form action="{{ route('user.update', $user_detail->id) }}" class="com-mail" method="post">
+                                        @csrf
                                         <label>Id</label>
-                                        <input type="text" name="id" value="" readonly/>
+                                        <input type="text" name="id" value="{{$user_detail->id}}" readonly/>
                                         <label>Họ và tên</label>
                                         <input type="text" name="name" value="{{$user_detail->name}}" required="required" />
                                         <label>Username</label>
