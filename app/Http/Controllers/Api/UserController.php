d14265eac7ad3f12b924f258e55ba84255fec894
Phulegia2020 <67860693+Phulegia2020@users.noreply.github.com>
2021-05-29 22:22:44 +0700

    Fix

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $user = User::all();
+        $user = User::all()->sortAsc();
         //return response()->json($user);
         return response()->json(['status' => 1, 'data' => UserResource::collection($user)]);
     }
