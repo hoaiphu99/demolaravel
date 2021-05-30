34ce25404bf4b5d472c74bd44b96514bdc1d3db0
Phulegia2020 <lgbphongphu2017@gmail.com>
2021-05-30 16:54:21 +0700

    Fix

--- resources/views/admin/user_update.blade.php
+++ resources/views/admin/user_update.blade.php
@@ -111,24 +111,44 @@
     </div>
 
     <script>
-    // Disable form submissions if there are invalid fields
-    (function() {
-    'use strict';
-    window.addEventListener('load', function() {
-        // Get the forms we want to add validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function () {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
         // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-        });
-    }, false);
-    })();
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+            })
+        })()
+        // Disable form submissions if there are invalid fields
+        // (function() {
+        // 'use strict';
+        // window.addEventListener('load', function() {
+        //     // Get the forms we want to add validation styles to
+        //     var forms = document.getElementsByClassName('needs-validation');
+        //     // Loop over them and prevent submission
+        //     var validation = Array.prototype.filter.call(forms, function(form) {
+        //     form.addEventListener('submit', function(event) {
+        //         if (form.checkValidity() === false) {
+        //             event.preventDefault();
+        //             event.stopPropagation();
+        //         }
+        //         form.classList.add('was-validated');
+        //     }, false);
+        //     });
+        // }, false);
+        // })();
     </script>
 </body>
 </html>
