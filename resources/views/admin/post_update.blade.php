8859a08b7f8034b1dcfbea42cc1d9d88424d8d33
Phulegia2020 <lgbphongphu2017@gmail.com>
2021-05-30 21:52:30 +0700

    Fix

--- resources/views/admin/post_update.blade.php
+++ resources/views/admin/post_update.blade.php
@@ -59,16 +59,18 @@
                     @csrf
                     <div class="form-group">
                         <label for="nd">Nội Dung:</label>
-                        <!-- <input type="text" class="form-control" id="nd" placeholder="Enter Content" name="content" value="{{$p->content}}" required="required"/> -->
-                        <textarea class="form-control" id="nd" rows="5" placeholder="write something" name="content" value="{{$p->content}}" required="required"></textarea>
+                        <input type="text" class="form-control" id="nd" placeholder="Enter Content" name="content" value="{{$p->content}}" required="required"/>
+                        <!-- <textarea class="form-control" id="nd" rows="5" placeholder="write something" name="content" value="{{$p->content}}" required="required"></textarea> -->
                         <!-- <div class="valid-feedback">Valid.</div> -->
                         <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
                     </div>
-                    <div class="form-group col-xs-3">
-                        <label for="uname">Người Đăng:</label>
-                        <input type="text" class="form-control" id="uname" placeholder="Enter username" name="user_id" value="{{$p->user->name}}" required="required"/>
-                        <!-- <div class="valid-feedback">Valid.</div> -->
-                        <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
+                    <div class="form-group">
+                        <div class="col-xs-3">
+                            <label for="uname">Người Đăng:</label>
+                            <input type="text" class="form-control" id="uname" placeholder="Enter username" name="user_id" value="{{$p->user->name}}" required="required"/>
+                            <!-- <div class="valid-feedback">Valid.</div> -->
+                            <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
+                        </div>
                     </div>
                     <div class="form-group">
                         <label for="image">Hình Ảnh:</label>
