1140242c13c7c2b5743f381370620cf8e03e23d7
Phulegia2020 <lgbphongphu2017@gmail.com>
2021-05-30 18:47:56 +0700

    Fix

--- resources/views/admin/post_update.blade.php
+++ resources/views/admin/post_update.blade.php
@@ -60,13 +60,13 @@
                     <div class="form-group">
                         <label for="nd">Nội Dung:</label>
                         <!-- <input type="text" class="form-control" id="nd" placeholder="Enter Content" name="content" value="{{$p->content}}" required="required"/> -->
-                        <textarea id="nd" rows="2" placeholder="write something" name="content" value="{{$p->content}}" required="required"></textarea>
+                        <textarea class="form-control" id="nd" rows="5" placeholder="write something" name="content" value="{{$p->content}}" required="required"></textarea>
                         <!-- <div class="valid-feedback">Valid.</div> -->
                         <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
                     </div>
-                    <div class="form-group">
+                    <div class="form-group col-xs-3">
                         <label for="uname">Người Đăng:</label>
-                        <input type="text" class="form-control" id="uname" placeholder="Enter username" name="user_id" value="{{$p->user->name}}" required="required" width="300"/>
+                        <input type="text" class="form-control" id="uname" placeholder="Enter username" name="user_id" value="{{$p->user->name}}" required="required"/>
                         <!-- <div class="valid-feedback">Valid.</div> -->
                         <!-- <div class="invalid-feedback">Please fill out this field.</div> -->
                     </div>
