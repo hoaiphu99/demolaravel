e190f2254b8342f86cdc12f9fe1184e5b2040f20
Phulegia2020 <lgbphongphu2017@gmail.com>
2021-05-30 17:15:38 +0700

    Fix

--- resources/views/admin/user_update.blade.php
+++ resources/views/admin/user_update.blade.php
@@ -10,7 +10,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
-  <link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/bootstrap/css/bootstrap.min.css') }}">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
