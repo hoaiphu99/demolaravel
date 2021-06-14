60041a6295f66a4567e36f0ed4ed3b3c976b6e19
Phulegia2020 <67860693+Phulegia2020@users.noreply.github.com>
2021-06-14 19:00:21 +0700

    Fix

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -183,7 +183,7 @@
                                                                     @csrf
                                                                     <input name="user_id" type="hidden" value="{{session()->get('user')->id}}">
                                                                     <input name="post_id" type="hidden" value="{{$p->id}}">
-                                                                    <button type = "submit" class="btn btn-light" class="like" data-toggle="tooltip" title="like">
+                                                                    <button type = "submit" class="like" data-toggle="tooltip" title="like" class="btn btn-link">
                                                                         <!-- <span class="like" data-toggle="tooltip" title="like"> -->
                                                                             <i class="ti-heart"></i>
                                                                             <ins>{{$p->like_count}}</ins>
