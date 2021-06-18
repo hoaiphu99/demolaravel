7e9717ff9d411b78309c3f66c4090ebd15915845
Phulegia2020 <67860693+Phulegia2020@users.noreply.github.com>
2021-06-18 23:50:46 +0700

    Fix

--- resources/views/admin/user_update.blade.php
+++ resources/views/admin/user_update.blade.php
@@ -161,10 +161,10 @@
         })();
     </script>
 
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
     <script src="{{secure_asset('assets/user/js/main.min.js')}}"></script>
-    <script src="{{secure_asset('assets/user/js/script.js')}}"></script> -->
+    <script src="{{secure_asset('assets/user/js/script.js')}}"></script>
 </html>
 
 <!-- Page-header start -->
