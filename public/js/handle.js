f7c46bcedd0e688223bb259c4648d240da0118b2
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-06-26 17:41:42 +0700

    fix

--- public/js/handle.js
+++ public/js/handle.js
@@ -199,23 +199,22 @@ const postUser = (data) => {
             const tbodyElement = document.querySelector("#list-user")
 
             const textNode = `<tr>
-                        <th scope="row">${result.data.id}</th>
-                        <td>${result.data.username}</td>
-                        <td>${result.data.name}</td>
-                        <td>${result.data.email}}</td>
-                        <td>${result.data.phone}</td>
-                        <td>${result.data.birthday}</td>
-                        <td><a href="/${result.data.id}"><b>Sửa</b></a></td>
+                        <th scope="row">${result.data[0].id}</th>
+                        <td>${result.data[0].username}</td>
+                        <td>${result.data[0].name}</td>
+                        <td>${result.data[0].email}}</td>
+                        <td>${result.data[0].phone}</td>
+                        <td>${result.data[0].birthday}</td>
+                        <td><a href="/${result.data[0].id}"><b>Sửa</b></a></td>
                         <td>
-                            <form action="/delete/${result.data.id}" method="post">
-                                ${@csrf}
+                            <form action="/delete/${result.data[0].id}" method="post">
                                 <input class="btn btn-danger" type="submit" value="Xóa"/>
                             </form>
                         </td>
                         </tr>`
 
             tbodyElement.innerHTML += textNode
-                console.log(result.data)
+                console.log(result.data[0])
         })
         .catch(error => {
             console.log(error)
