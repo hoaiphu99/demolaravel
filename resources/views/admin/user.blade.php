58340ec179f263dbfeddb2983576db3429c73317
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-06-26 16:01:43 +0700

    test form validator

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -17,16 +17,51 @@
             <div>
                 <button type="button" class="btn btn-primary btn-insert" >Thêm</button>
             </div>
+            <hr>
             <div class="insert-user">
-                <form action="{{ route('user.create') }}" method="post">
+                <form method="post" id="form-add-user">
                     @csrf
-                    <input type="text" class="form-control" name="username" required="required">
-                    <input type="text" class="form-control" name="password" required="required">
-                    <input type="text" class="form-control" name="name" required="required">
-                    <input type="text" class="form-control" name="email" required="required">
-                    <input type="text" class="form-control" name="phone" required="required">
-                    <input type="text" class="form-control" name="birthday" required="required">
-                    {{--<td><i class="fa fa-pencil"><button class="btn btn-primary btn-submit-user" type="submit">Lưu</button></i>--}}
+                    <div class="form-group">
+                        <label for="username" class="form-label">Tên tài khoản</label>
+                        <input id="username" name="username" type="text" placeholder="Nhập tên tài khoản" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="form-label">Mật khẩu</label>
+                        <input id="password" name="password" type="password" placeholder="Nhập mật khẩu" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation" class="form-label">Nhập lại mật khẩu</label>
+                        <input id="password_confirmation" name="password_confirmation" placeholder="Nhập lại mật khẩu"
+                               type="password" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="name" class="form-label">Họ tên</label>
+                        <input id="name" name="name" type="text" placeholder="Nguyễn Văn A" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="form-label">Email</label>
+                        <input id="email" name="email" type="text" placeholder="email@domain.com" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="phone" class="form-label">Số điện thoại</label>
+                        <input id="phone" name="phone" type="number" placeholder="0123456789" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="birthday" class="form-label">Ngày tháng năm sinh</label>
+                        <input id="birthday" name="birthday" type="text" placeholder="VD: username" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="avatar" class="form-label">Ảnh đại diện</label>
+                        <input id="avatar" name="avatar" type="file" class="form-control">
+                        <span class="form-message"></span>
+                    </div>
                     <button class="btn btn-primary btn-submit-user" type="submit">Lưu</button>
                 </form>
             </div>
@@ -101,4 +136,29 @@
 </div>
 <!-- Page-body end -->
 </div>
+<script>
+    Validator({
+        form: '#form-add-user',
+        formGroupSelector: '.form-group',
+        errorSelector: '.form-message',
+        rules: [
+            Validator.isRequired('#name', 'Vui lòng nhập tên đầy đủ của bạn'),
+            Validator.isRequired('#email', 'Vui lòng nhập email'),
+            Validator.isEmail('#email'),
+            Validator.minLength('#password', 6),
+            Validator.isRequired('#password_confirmation'),
+            Validator.isConfirmed('#password_confirmation', function () {
+                return document.querySelector('#form-add-user #password').value
+            }, 'Mật khẩu nhập lại không chính xác'),
+            // Validator.isRequired('input[name="gender"]', 'Vui lòng chọn giới tính'),
+            // Validator.isRequired('input[name="language"]', 'Vui lòng chọn ngôn ngữ'),
+            // Validator.isRequired('#province', 'Vui lòng chọn Tỉnh/TP'),
+            Validator.isRequired('#avatar', 'Vui lòng chọn ảnh đại diện'),
+        ],
+        onSubmit: function(data) {
+            // Call API
+            console.log(data)
+        }
+    })
+</script>
 @endsection
