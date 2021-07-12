6c96a61c72794c46f76e580a8a29886a0acf0e7c
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-07-12 17:56:46 +0700

    Update index.blade.php

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -195,39 +195,39 @@
 {{--                                                                    </button>--}}
 {{--                                                                </form>--}}
                                                             </li>
-                                                            <li class="social-media">
-                                                                <div class="menu">
-                                                                    <div class="btn trigger"><i class="fa fa-share-alt"></i></div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-html5"></i></a></div>
-                                                                    </div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-facebook"></i></a></div>
-                                                                    </div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-google-plus"></i></a></div>
-                                                                    </div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-twitter"></i></a></div>
-                                                                    </div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-css3"></i></a></div>
-                                                                    </div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-instagram"></i></a>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-dribbble"></i></a>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="rotater">
-                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-pinterest"></i></a>
-                                                                        </div>
-                                                                    </div>
+{{--                                                            <li class="social-media">--}}
+{{--                                                                <div class="menu">--}}
+{{--                                                                    <div class="btn trigger"><i class="fa fa-share-alt"></i></div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-html5"></i></a></div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-facebook"></i></a></div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-google-plus"></i></a></div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-twitter"></i></a></div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-css3"></i></a></div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-instagram"></i></a>--}}
+{{--                                                                        </div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-dribbble"></i></a>--}}
+{{--                                                                        </div>--}}
+{{--                                                                    </div>--}}
+{{--                                                                    <div class="rotater">--}}
+{{--                                                                        <div class="btn btn-icon"><a href="#" title=""><i class="fa fa-pinterest"></i></a>--}}
+{{--                                                                        </div>--}}
+{{--                                                                    </div>--}}
 
-                                                                </div>
-                                                            </li>
+{{--                                                                </div>--}}
+{{--                                                            </li>--}}
                                                         </ul>
                                                     </div>
                                                     <div class="description">
