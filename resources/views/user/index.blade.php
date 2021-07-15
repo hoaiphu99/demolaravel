dc20bde6dc0334176ad12264e424d0af12882c14
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-07-15 08:45:44 +0700

    Update index.blade.php

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -180,8 +180,8 @@
                                                             </li>
                                                             <li>
                                                                 <span class="like like-btn" onclick="likePost(event, {{ $p->id }}, {{ session()->get('user')->id }})" data-toggle="tooltip" title="Likes">
-                                                                    <i class="ti-heart "></i>
-                                                                    {{-- $p->status === 'liked' ? 'liked' : '' --}}
+                                                                    <i class="ti-heart {{$p->status === 'liked' ? 'liked' : '' }}"></i>
+
                                                                     <ins>{{$p->like_count}}</ins>
                                                                 </span>
 
