fc8d84efb08f827c8f75317745f098e207000893
Hoaiphu <hoaiphu.ptit@gmail.com>
2021-07-15 23:23:15 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -46,6 +46,7 @@ $(document).ready(function () {
         }
         else {
             const data = formCreate.serialize()
+            console.log(data)
             $.ajax({
                 type: 'POST',
                 url: `${url}/post/create`,
